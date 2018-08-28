5f214daab6f17ccba27e04e3d74ef5a894d6af78
xSchmittHappenZx <calebschmitt12@gmail.com>
2018-08-27 21:41:35 -0500

    FAILED: Demonstrating that port for integration failed

---
+++ src/test/java/xschmitthappenzx/contractpoc/ContractPocApplicationIntTest.java
@@ -0,0 +1,27 @@
+package xschmitthappenzx.contractpoc;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.assertj.core.api.BDDAssertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContractPocApplicationIntTest {
+
+    @Test
+    public void test_should_return_all_coins(){
+
+        String json = "[\"BTC\", \"ETH\"]";
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> entity = restTemplate.getForEntity("http://localhost:8081/coin", String.class);
+
+        BDDAssertions.then(entity.getStatusCodeValue()).isEqualTo(201);
+        BDDAssertions.then(entity.getBody()).isEqualTo(json);
+    }
+}
